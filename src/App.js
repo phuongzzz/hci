e4df480bb80700c94a231c958a169def919847a4
phuongzzz <yesterdayoncemore23@gmail.com>
2017-10-30 16:18:03 +0700

    add router and bootstrap, toastr and jquery

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
-class App extends Component {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+);
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link className="navbar-brand" to="/">WebSiteName</Link>
+            </div>
+            <ul className="nav navbar-nav">
+              <li><Link to="/category">Category</Link></li>
+              <li><Link to="/products">Products</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" component={Home}/>
+        <Route path="/category" component={Category}/>
+        <Route path="/products" component={Products}/>
+
       </div>
-    );
+    )
   }
 }
 
