019d97f483b8111e1cc8b6f61e815f2a957e60a0
phuongzzz <yesterdayoncemore23@gmail.com>
2017-11-14 10:56:58 +0700

    done quy #1

--- src/components/Responding/RespondingPage.js
+++ src/components/Responding/RespondingPage.js
@@ -5,51 +5,73 @@ import {Link} from 'react-router';
 const RespondingPage = React.createClass({
   render(){
     return(
-      <div id="row">
-          <div className="row-responding">
-            {/*forums company-responding*/}
-              <div className="col-md-10 col-md-offset-1">
-                  <h6>Company Responding Internship</h6>
-                  <table className="table company-table-respon table-hover table-sm table-striped table-fixed ">
-                      <thead>
-                      <tr>
-                          <th className="text-center">ID</th>
-                          <th className="text-center">Company</th>
-                      </tr>
-                      </thead>
-                      <tbody>
-                      {this.props.companies.map((company, i) =>
-                        <tr key={i}>
-                            <td>{company.id}</td>
-                            <td><Link to={`/company/${company.name}`}>{company.name}</Link></td>
-                        </tr>
-                      )}
-                      </tbody>
-                  </table>
-              </div>
-            {/*forums student-responding*/}
-              <div className="col-md-10 col-md-offset-1">
-                  <h6 className="h6-class-responding">Class Responding Internship</h6>
-                  <table className="table class-table-respon table-hover table-sm table-striped">
-                      <thead className="thead-class-respon">
-                      <tr>
-                          <th className="text-center">ID</th>
-                          <th className="text-center">Class</th>
-                      </tr>
-                      </thead>
-                      <tbody>
-                      {this.props.classes.map((classe , i) =>
-                        <tr key={i}>
-                            <td>{classe.id}</td>
-                            <td><Link to={`/classe/${classe.name}`}>{classe.name}</Link></td>
-                        </tr>
-                      )}
-                      </tbody>
-                  </table>
-              </div>
-          </div>
+      <div className="container">
+    <br/>
+    <h4 className="quy-panel-title">APPLICATION FOR ADVANCE</h4>
+    <hr/>
+    <div className="row col-md-8 col-md-offset-2">
+      <div className="row quy-date">
+        <div className="col-md-1 col-md-offset-6">Time</div>
+        <div className="col-md-4">
+          <input  className="form-control" type="date"/>
+        </div>
       </div>
-
+      <br/>
+      <div className="profile-right quy-user-info">
+        <div className="panel">
+            <div className="panel-body quy-panel-body">
+                <ul className="list-unstyled task-list">
+                    <li>
+                      <div className="row">
+                          <div className="col-md-2 col-md-offset-2"><span>Dear: </span></div>
+                          <div className="col-md-7"><span><input type="text" className="form-control" placeholder="Enter Name Here.."/></span></div>
+                      </div>
+                    </li>
+                    <hr/>
+                    <li>
+                      <div className="row">
+                          <div className="col-md-2 col-md-offset-2"><span>Full Name: </span></div>
+                          <div className="col-md-7"><span><input type="text" className="form-control" placeholder="Enter Your Full Name Here..."/></span></div>
+                      </div>
+                    </li>
+                    <hr/>
+                    <li>
+                      <div className="row">
+                          <div className="col-md-2 col-md-offset-2"><span>Part:</span></div>
+                          <div className="col-md-7"><span><input type="text" className="form-control" placeholder="Enter Your Part Here.."/></span></div>
+                      </div>
+                    </li>
+                    <hr/>
+                    <li>
+                    <div className="row">
+                      <div className="col-md-2 col-md-offset-2"><span>Estimate Cost</span></div>
+                      <div className="col-md-7"><span><input type="text" className="form-control" placeholder="Enter number cost..."/></span></div>
+                    </div>
+                    </li>
+                    <hr/>
+                    <li>
+                    <div className="row">
+                      <div className="col-md-2 col-md-offset-2"><span>Reason for advance:</span></div>
+                      <div className="col-md-7"><span><textarea type="text" className="form-control" placeholder="Enter Your Bank Account Here..."></textarea></span></div>
+                    </div>
+                    </li>
+                    <hr/>
+                    <li>
+                    <div className="row">
+                      <div className="col-md-2 col-md-offset-2"><span>Billing Period</span></div>
+                      <div className="col-md-7"><span><input type="text" className="form-control" placeholder="Enter time..."/></span></div>
+                    </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </div>
+      <div className="row col-md-6 col-md-offset-4" >
+        <button type="button" className="btn btn-success">Success</button>
+        <button type="button" className="btn btn-danger">Danger</button>
+      </div>
+    </div>
+  </div>
     )
   }
 
